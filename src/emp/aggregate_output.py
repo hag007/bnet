a53bcc68f6fb8a4ff92afadd940ad6c8f19ba7c7
David Pellow <dpellow@localhost.localdomain>
2021-08-05 15:59:14 +0300

    sync

---
+++ src/emp/aggregate_output.py
@@ -0,0 +1,84 @@
+import sys
+
+sys.path.insert(0, '../../')
+# sys.path.insert(0, '../')
+# sys.path.insert(0, '../../../')
+
+import os
+import subprocess
+import src.constants as constants
+
+import pandas as pd
+import argparse
+import json
+import itertools
+
+from multiprocessing.pool import Pool
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='args')
+    parser.add_argument('--dataset_files', dest='dataset_files', default=constants.config_json["dataset_files"])
+    parser.add_argument('--algo', dest='algo', default=constants.config_json["algo"])
+    parser.add_argument('--network_file', dest='network_file', default=constants.config_json["network_file"])
+    parser.add_argument('--go_folder', dest='go_folder', default=constants.config_json["go_folder"])
+    parser.add_argument('--true_solutions_folder', dest='true_solutions_folder',
+                        default=constants.config_json["true_solutions_folder"])
+    parser.add_argument('--metric_folder', dest='metric_folder', default=constants.config_json["metric_folder"])
+    parser.add_argument('--pf', dest='pf', help="parallelization factor", default=constants.config_json["pf"])
+    parser.add_argument('--additional_args', help="additional_args", dest='additional_args',
+                        default=constants.config_json["additional_args"])
+    parser.add_argument('--processes', dest="processes", help='generate_solution,calc_pcs,calc_prediction',
+                        default=constants.config_json["processes"])
+    parser.add_argument('--tuning_comb', dest="tuning_comb", help='',
+                        default=constants.config_json["tuning_comb"])
+    args = parser.parse_args()
+    dataset_files = args.dataset_files
+    algo = args.algo
+    true_solutions_folder = args.true_solutions_folder
+    tuning_comb_args = args.tuning_comb
+
+    tuning_comb = json.loads(str(tuning_comb_args))
+    combs = list(itertools.product(*tuning_comb.values()))
+    p = Pool(30)
+    params = []
+    combs_str = "report"
+    for dataset_file in dataset_files:
+        dataset_name = os.path.splitext(os.path.split(dataset_file)[1])[0]
+        for comb in combs:
+            tuning_comb
+            params.append([dataset_name, algo, true_solutions_folder, comb])
+            combs_str += "_" + "_".join([dataset_name, algo] + [str(tuning_comb[a]) for a in \
+                                                                ["ts", "min_temp", "temp_factor", "slice_threshold",
+                                                                 "module_threshold", "sim_factor",
+                                                                 "activity_baseline"]])
+    results = p.map(fetch_metrics, params)
+
+    df = pd.DataFrame()
+    for result in results:
+        df = df.append(result)
+
+    metrics_folder = os.path.join(true_solutions_folder, "metrics")
+    if not os.path.isdir(metrics_folder):
+        os.makedirs(metrics_folder)
+    fname = os.path.join(metrics_folder, f"{combs_str}.csv")
+    df.to_csv(fname)
+
+
+def fetch_metrics(args):
+    dataset_name, algo, true_solutions_folder, comb = args
+    tuning_args = {k: v for k, v in zip(tuning_comb.keys(), comb)}
+    params_name = "_".join([str(tuning_args[a]) for a in \
+                            ["ts", "min_temp", "temp_factor", "slice_threshold", "module_threshold", "sim_factor",
+                             "activity_baseline"]])
+
+    result_folder = os.path.join(true_solutions_folder, "{}_{}_{}".format(dataset_name, algo, params_name))
+    report_file = os.path.join(result_folder, "report.tsv")
+    df = pd.read_csv(report_file)
+    return df.iloc[0]
+
+
+if __name__ == "__main__":
+    main()
--- src/emp/run_sequentially.py
+++ src/emp/run_sequentially.py
@@ -10,6 +10,9 @@ import src.constants as constants
 import pandas as pd
 import argparse
 import json
+import itertools
+
+from multiprocessing.pool import Pool
 
 current_path=os.path.dirname(os.path.realpath(__file__))
 
@@ -24,14 +27,11 @@ def execute_stage(py_script, params):
     # print(out)
     while True:
         output = prc.stdout.readline()
-        if output == b'' and prc.poll() is not None:
+        if output == b'': #  and prc.poll() is not None:
             break
         if output:
             out_str=output.decode("utf-8")
-            if out_str.endswith("@\n"):
-                print('\r'+out_str[:-2], end ='')
-            else:
-                print(out_str, end ='')
+            print(out_str, end ='')
 
     rc = prc.poll()
 
@@ -43,18 +43,19 @@ def execute_stage(py_script, params):
 def main():
 
     parser = argparse.ArgumentParser(description='args')
-    parser.add_argument('--dataset_file', dest='dataset_file', default=constants.config_json["dataset_file"])
+    parser.add_argument('--dataset_files', dest='dataset_files', default=constants.config_json["dataset_files"])
     parser.add_argument('--algo', dest='algo', default=constants.config_json["algo"])
     parser.add_argument('--network_file', dest='network_file', default=constants.config_json["network_file"])
     parser.add_argument('--go_folder', dest='go_folder', default=constants.config_json["go_folder"])
     parser.add_argument('--true_solutions_folder', dest='true_solutions_folder', default=constants.config_json["true_solutions_folder"])
-    parser.add_argument('--report_folder', dest='report_folder', default=constants.config_json["report_folder"])
     parser.add_argument('--pf', dest='pf', help="parallelization factor", default=constants.config_json["pf"])
     parser.add_argument('--additional_args', help="additional_args", dest='additional_args', default=constants.config_json["additional_args"])
     parser.add_argument('--processes', dest="processes", help='generate_solution,calc_pcs,calc_prediction', default=constants.config_json["processes"])
+    parser.add_argument('--tuning_comb', dest="tuning_comb", help='',
+                        default=constants.config_json["tuning_comb"])
     args = parser.parse_args()
 
-    dataset_file=args.dataset_file
+    dataset_files=args.dataset_files
     algo=args.algo
     network_file=args.network_file
     go_folder=args.go_folder
@@ -62,37 +63,54 @@ def main():
     report_folder=args.report_folder
     pf=args.pf
     additional_args = args.additional_args
+    tuning_comb_args = args.tuning_comb
     processes = args.processes.split(",")
 
-    dataset_file_params = "--dataset_file {}".format(dataset_file)
+
     algo_param="--algo {}".format(algo)
     true_solutions_folder_param = "--true_solutions_folder {}".format(true_solutions_folder)
     report_folder_param = "--report_folder {}".format(report_folder)
     network_file_param = "--network_file {}".format(network_file)
     go_folder_param = "--go_folder {}".format(go_folder)
     pf_param = "--pf {}".format(pf)
-    additional_args_param = "--additional_args {}".format(json.dumps(str(additional_args)))
+    tuning_comb = json.loads(str(tuning_comb_args))
+    combs=list(itertools.product(*tuning_comb.values()))
+    p=Pool(30)
+    params=[]
+    for dataset_file in dataset_files:
+        dataset_file_params = "--dataset_file {}".format(dataset_file)
+        for comb in combs:
+            params.append([additional_args, algo_param, comb, dataset_file_params, go_folder_param,
+            network_file_param, processes, true_solutions_folder_param, tuning_comb])
+    p.map(execute_one_series,params)
 
-    params_by_processes={
+
+def execute_one_series(args):
+    additional_args, algo_param, comb, dataset_file_params, go_folder_param, network_file_param, processes, true_solutions_folder_param, tuning_comb = args
+    tuning_args = {k: v for k, v in zip(tuning_comb.keys(), comb)}
+    additional_args = json.loads(additional_args)
+    additional_args.update(tuning_args)
+    print(additional_args)
+    additional_args = json.dumps(additional_args)
+    additional_args_param = "--additional_args {}".format(json.dumps(str(additional_args)))
+    params_by_processes = {
         "generate_solution": [dataset_file_params, algo_param, network_file_param, go_folder_param,
                               true_solutions_folder_param, additional_args_param],
         "calc_pcs": [dataset_file_params, algo_param, network_file_param, go_folder_param,
-                              true_solutions_folder_param, additional_args_param],
+                     true_solutions_folder_param, additional_args_param],
         "calc_prediction": [dataset_file_params, algo_param, network_file_param, go_folder_param,
-                              true_solutions_folder_param, additional_args_param]}
-
+                            true_solutions_folder_param, additional_args_param]}
     for cur_process in processes:
         if cur_process not in params_by_processes:
             print("unknown process detected: {}. abort...".format(cur_process))
             raise Exception
-
     try:
-       for cur_process in processes:
-           execute_stage(cur_process+".py", params_by_processes[cur_process])
+        for cur_process in processes:
+            execute_stage(cur_process + ".py", params_by_processes[cur_process])
 
     except Exception as e:
-       print("error in {}: {}".format(cur_process, e))
-       raise
+        print("error in {}: {}".format(cur_process, e))
+        raise
 
 
 if __name__=="__main__":
