1af468ba7fa242f8a05b2608a5204a3d141aa2e7
David Pellow <dpellow@localhost.localdomain>
2021-08-22 14:55:22 +0300

    Add jAM runner

--- src/emp/generate_solution.py
+++ src/emp/generate_solution.py
@@ -13,6 +13,7 @@ def main():
     parser = argparse.ArgumentParser(description='args')
     parser.add_argument('--dataset_file', dest='dataset_file', help='/path/to/dataset_file', default=constants.config_json["dataset_file"])
     parser.add_argument('--algo', dest='algo', default=constants.config_json["algo"])
+    parser.add_argument('--compare', dest='compare', default=constants.config_json["compare_algo"])
     parser.add_argument('--network_file', dest='network_file', help='/path/to/network_file', default=constants.config_json["network_file"])
     parser.add_argument('--go_folder', dest='go_folder', default=constants.config_json["go_folder"])
     parser.add_argument('--true_solutions_folder', dest='true_solutions_folder', default=constants.config_json["true_solutions_folder"])
@@ -21,28 +22,34 @@ def main():
 
     dataset_file=args.dataset_file
     algo=args.algo
+    compare_algo = args.compare
     network_file = args.network_file
     go_folder = args.go_folder
     true_solutions_folder = args.true_solutions_folder
-    additional_args = args.additional_args
 
     init_state(go_folder)
 
     additional_args_json = json.loads(args.additional_args)
 
+
     params_name = "_".join([str(additional_args_json[a]) for a in \
                                 ["ts", "min_temp", "temp_factor", "slice_threshold", "module_threshold", "sim_factor", "activity_baseline"]])
+
     # read files
     dataset_name=os.path.splitext(os.path.split(dataset_file)[1])[0]
     network_name=os.path.splitext(os.path.split(network_file)[1])[0]
     output_folder=os.path.join(true_solutions_folder, "{}_{}_{}_{}".format(dataset_name,network_name,algo,params_name))
+
+    if compare_algo is not None:
+        compare_folder = os.path.join(true_solutions_folder, "{}_{}_{}_{}".format(dataset_name,network_name,compare_algo,params_name))
+        additional_args_json['compare'] = compare_folder
 
     try:
         os.makedirs(output_folder)
     except FileExistsError:
         pass
 
-    run_algo(dataset_file, algo, network_file, go_folder, os.path.join(output_folder) , **json.loads(additional_args))
+    run_algo(dataset_file, algo, network_file, go_folder, os.path.join(output_folder) , **additional_args_json)
 
 
 if __name__ == "__main__":
--- src/runners/jactivemodules_greedy_runner.py
+++ src/runners/jactivemodules_greedy_runner.py
@@ -15,6 +15,7 @@ import src.constants as constants
 
 from src.utils.scripts import format_script
 from src.utils.network import get_network_genes
+from src.utils.modules import get_num_modules
 
 from src.runners.abstract_runner import AbstractRunner
 class jAMGreedyRunner(AbstractRunner):
@@ -22,10 +23,11 @@ class jAMGreedyRunner(AbstractRunner):
         super().__init__("jactivemodules")
         self.search_method="greedy"
 
-    def init_params(self, network_file_name, output_folder):
+    def init_params(self, network_file_name, output_folder, compare_folder):
+        num_modules = get_num_modules(compare_folder)
         bg_genes=get_network_genes(network_file_name)
         results_file_name = "{}/{}_{}_results.txt".format(output_folder, self.ALGO_NAME, self.search_method)
-        return results_file_name, bg_genes
+        return results_file_name, bg_genes, num_modules
 
 
     def extract_modules_and_bg(self, bg_genes, results_file_name, modules_genes_file_name, output_folder):
@@ -42,13 +44,13 @@ class jAMGreedyRunner(AbstractRunner):
 
     def run(self, dataset_file_name, network_file_name, output_folder, **kwargs):
         print("start running jactivemodules_greedy")
-        results_file_name, bg_genes = self.init_params(network_file_name, output_folder)
+        compare_folder = kwargs["compare"]
+        results_file_name, bg_genes, num_of_modules = self.init_params(network_file_name, output_folder,compare_folder)
         script_file_name = format_script(os.path.join(constants.dir_path, "src/sh", "run_{}.sh".format(self.ALGO_NAME)),
-                                         BASE_FOLDER="/specific/netapp5/gaga/hagailevi/emp_test",
                                          ALGO_DIR=self.ALGO_DIR, NETWORK_FILE_NAME=network_file_name,
                                          SCORE_FILE_NAME=dataset_file_name,
                                          IS_GREEDY=str(self.search_method == "greedy"), OUTPUT_FILE=results_file_name,
-                                         NUM_OF_MODULES=50, OVERLAP_THRESHOLD=0)
+                                         NUM_OF_MODULES=num_of_modules, OVERLAP_THRESHOLD=0)
         print("running :{}".format(script_file_name))
         subprocess.Popen("bash {}".format(script_file_name), shell=True,
                          stdout=subprocess.PIPE, cwd=self.ALGO_DIR).stdout.read()
